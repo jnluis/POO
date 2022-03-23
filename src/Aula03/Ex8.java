becf12bf111034cca501f9363d6cbec88aa78f30
jnluis <jnluis@ua.pt>
2022-03-23 17:03:52 +0000

    Alteração do ex. 8 da ficha 3

--- src/Aula03/Ex8.java
+++ src/Aula03/Ex8.java
@@ -1,49 +1,28 @@
 package Aula03;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Ex8 {
     public static void main(String[] args){
-        double CP;
-        double CT;
-        double N;
+        Double CP, CT,N;
         Scanner sc = new Scanner(System.in);
-        ArrayList<Double> NCP = new ArrayList<>();
-        ArrayList<Double> NCT = new ArrayList<>();
-        ArrayList<Double> NF = new ArrayList<>();
+        Double[][] turma = new Double[16] [3];
 
-
-        for(int i=1; i<=16; i++){
-
-            System.out.println("Notas Aluno "+ i + " -");
-            System.out.println("Insira a nota da componente prática: ");
-            CP = sc.nextDouble();
-            if(CP < 0 || CP>20){
-                System.out.println("Insira uma nota válida!!");
-                System.out.print("Insira a nota da componente prática: ");
-                CP = sc.nextDouble();
-            }
-            System.out.println("Insira a nota da componente teórica: ");
-            CT = sc.nextDouble();
-            if(CP < 0 || CP>20){
-                System.out.println("Insira uma nota válida!!");
-                System.out.print("Insira a nota da componente teórica:");
-                CP = sc.nextDouble();
-            }
+        for (int i = 0; i < 16; i++){
+            CP = Math.floor(Math.random()*21);
+            CT = Math.floor(Math.random()*21);
 
             N = (0.4*CT) + (0.6*CP);
-            if(CP < 7.0 || CT < 7.0 ){
-                N = 66;
+            if (CT <7.0 || CP < 7.0){
+                N = 66.0;
             }
-            NCP.add(CP);
-            NCT.add(CT);
-            NF.add(N);
+            turma[i][0] = (double) Math.round(CP);
+            turma[i][1] = (double) Math.round(CT);
+            turma[i][2] = (double) Math.round(N);
         }
         System.out.printf("%-7s  %-7s %-7s %n","NotaT","NotaP","Pauta");
         for(int i=0; i<16; i++){
-            System.out.format("%-7.1f  %-7.1f %-7.0f %n", NCP.get(i), NCT.get(i), NF.get(i));
+            System.out.format("%-7.1f  %-7.1f %-7.0f %n", turma[i][1], turma[i][0], turma[i][2]);
         }
         sc.close();
     }
 }
-
